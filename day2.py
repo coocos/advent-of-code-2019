e737c776b4206e6ba29e17d3c58001641242c453
Kalle Lämsä <1397804+coocos@users.noreply.github.com>
2019-12-02 18:22:39 +0200

    Solve day 2 puzzles

---
+++ day2.py
@@ -0,0 +1,62 @@
+import sys
+from typing import List
+from enum import Enum
+
+
+class IntcodeException(Exception):
+    pass
+
+
+class Opcode(Enum):
+
+    ADD = 1
+    MULTIPLY = 2
+    HALT = 99
+
+
+def execute(program: List[int]) -> List[int]:
+
+    pointer = 0
+    opcode = Opcode(program[pointer])
+
+    while opcode != Opcode.HALT:
+
+        a, b, c = program[pointer + 1 : pointer + 4]
+
+        if opcode == Opcode.ADD:
+            program[c] = program[a] + program[b]
+        elif opcode == Opcode.MULTIPLY:
+            program[c] = program[a] * program[b]
+        else:
+            raise IntcodeException(f"Unhandled instruction {opcode}")
+
+        pointer += 4
+        opcode = Opcode(program[pointer])
+
+    return program
+
+
+if __name__ == "__main__":
+
+    with open("day2.in") as f:
+        program = [int(code) for code in f.read().split(",")]
+
+    # First part
+    copy = program[:]
+    copy[1] = 12
+    copy[2] = 2
+    executed = execute(copy)
+    assert executed[0] == 3409710
+
+    # Second part
+    for noun in range(100):
+        for verb in range(100):
+            copy = program[:]
+            copy[1] = noun
+            copy[2] = verb
+            executed = execute(copy)
+            if executed[0] == 19690720:
+                assert 100 * noun + verb == 7912
+                sys.exit()
+
+    assert False
