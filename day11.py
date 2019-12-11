a231a97396f469fdef278c8bf602c2de4ee59a5d
Kalle Lämsä <1397804+coocos@users.noreply.github.com>
2019-12-11 12:30:56 +0200

    Clean up day 11

--- day11.py
+++ day11.py
@@ -25,7 +25,7 @@ class Robot:
     pos: Vector = Vector(0, 0)
     dir: Vector = Vector(0, 1)
 
-    def turn_left(self):
+    def turn_left(self) -> None:
 
         if self.dir == Vector(0, 1):
             self.dir = Vector(-1, 0)
@@ -38,7 +38,7 @@ class Robot:
 
         self.pos = Vector(self.pos.x + self.dir.x, self.pos.y + self.dir.y)
 
-    def turn_right(self):
+    def turn_right(self) -> None:
         if self.dir == Vector(0, 1):
             self.dir = Vector(1, 0)
         elif self.dir == Vector(1, 0):
@@ -51,15 +51,14 @@ class Robot:
         self.pos = Vector(self.pos.x + self.dir.x, self.pos.y + self.dir.y)
 
 
-if __name__ == "__main__":
+def paint_panels(
+    program: List[int], starting_panel: Panel
+) -> DefaultDict[Vector, Panel]:
 
-    with open(os.path.join("inputs", "day11.in")) as f:
-        program = [int(byte) for byte in f.read().strip().split(",")]
-
-    # First part
     robot = Robot()
     panels: DefaultDict[Vector, Panel] = collections.defaultdict(lambda: Panel.BLACK)
 
+    panels[robot.pos] = starting_panel
     input_stream: List[int] = [panels[robot.pos]]
     machine = Machine(program, input_stream, pause_on_output=True)
 
@@ -82,34 +81,20 @@ if __name__ == "__main__":
 
         input_stream.append(panels[robot.pos])
 
+    return panels
+
+
+if __name__ == "__main__":
+
+    with open(os.path.join("inputs", "day11.in")) as f:
+        program = [int(byte) for byte in f.read().strip().split(",")]
+
+    # First part
+    panels = paint_panels(program, Panel.BLACK)
     assert len(panels) == 2016
 
     # Second part
-    robot = Robot()
-    panels = collections.defaultdict(lambda: Panel.BLACK)
-
-    panels[robot.pos] = Panel.WHITE
-    input_stream = [panels[robot.pos]]
-    machine = Machine(program, input_stream, pause_on_output=True)
-
-    while not machine.halted:
-
-        machine.execute()
-        machine.execute()
-
-        if machine.halted:
-            break
-
-        direction = machine.output.pop()
-        panel = Panel(machine.output.pop())
-        panels[robot.pos] = panel
-
-        if direction == 0:
-            robot.turn_left()
-        else:
-            robot.turn_right()
-
-        input_stream.append(panels[robot.pos])
+    panels = paint_panels(program, Panel.WHITE)
 
     xs = sorted(panels.keys(), key=attrgetter("x"))
     ys = sorted(panels.keys(), key=attrgetter("y"))
