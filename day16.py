fc06f4db6624d7c829a37e6c7ac3bcbd27d39f26
Kalle Lämsä <1397804+coocos@users.noreply.github.com>
2020-01-13 20:42:00 +0200

    Solve second part of day 16

--- day16.py
+++ day16.py
@@ -36,9 +36,45 @@ def fft(signal: str, phases: int = 1) -> str:
     return "".join(str(s) for s in phased)
 
 
+def offset_fft(signal: str, phases: int = 1) -> str:
+
+    phased = [int(s) for s in signal]
+    phased_sums = [-1 for _ in phased]
+
+    for i in range(phases):
+
+        print(f"Running phase {i}")
+
+        # Calculate cumulative sums for the previous phase
+        pos = len(signal) - 1
+        while pos >= 0:
+            try:
+                phased_sums[pos] = phased[pos] + phased_sums[pos + 1]
+            except IndexError:
+                phased_sums[pos] = phased[pos]
+            pos -= 1
+
+        new = [0 for _ in signal]
+        pos = len(signal) - 1
+        while pos >= 0:
+            # Next phase is modulo 10 of the sums from the previous phase
+            new[pos] = phased_sums[pos] % 10
+            pos -= 1
+
+        phased = new
+
+    return "".join(str(s) for s in phased)
+
+
 if __name__ == "__main__":
 
     with open(os.path.join("inputs", "day16.in")) as f:
         signal = f.read().strip()
 
+    # First part
     assert fft(signal, 100)[:8] == "17978331"
+
+    # Second part
+    signal *= 10000
+    offset = int(signal[:7])
+    assert offset_fft(signal[offset:], 100)[:8] == "19422575"
