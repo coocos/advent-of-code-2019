1b53acb321cb3fc717cebe3df7e67f890dff79fb
Kalle Lämsä <1397804+coocos@users.noreply.github.com>
2020-01-27 20:33:14 +0200

    Clean up day 12

--- day12.py
+++ day12.py
@@ -4,7 +4,7 @@ import re
 import math
 import itertools
 from dataclasses import dataclass
-from typing import List
+from typing import List, Tuple, Set, Dict
 
 
 @dataclass
@@ -17,13 +17,20 @@ class Vec:
     def __add__(self, vec: Vec) -> Vec:
         return Vec(self.x + vec.x, self.y + vec.y, self.z + vec.z)
 
+    def __getitem__(self, key: str) -> int:
+        assert key in "xyz"
+        return getattr(self, key)
+
+    def __setitem__(self, key: str, value: int) -> None:
+        assert key in "xyz"
+        setattr(self, key, value)
+
 
 @dataclass
 class Moon:
 
     pos: Vec
-    vel: Vec  # TODO: Use field / factorymethod to set this by default
-    name: str
+    vel: Vec
 
     def update(self) -> None:
         self.pos = self.pos + self.vel
@@ -35,11 +42,25 @@ class Moon:
         )
 
 
-def lcm(x, y, z) -> int:
+def lcm(x: int, y: int, z: int) -> int:
 
     a = x * y // math.gcd(x, y)
     b = a * z // math.gcd(a, z)
     return b
+
+
+def parse_moons(input_list: List[str]) -> List[Moon]:
+
+    pattern = r"<x=([0-9-]+), y=([0-9-]+), z=([0-9-]+)>"
+    moons: List[Moon] = []
+    for line in input_list:
+        match = re.match(pattern, line)
+        if match:
+            moon = Moon(Vec(*[int(coord) for coord in match.groups()]), Vec(0, 0, 0),)
+            moons.append(moon)
+        else:
+            raise Exception(f"Unable to parse {line}")
+    return moons
 
 
 if __name__ == "__main__":
@@ -47,47 +68,21 @@ if __name__ == "__main__":
     with open(os.path.join("inputs", "day12.in")) as f:
         lines = f.readlines()
 
-    pattern = r"<x=([0-9-]+), y=([0-9-]+), z=([0-9-]+)>"
-    moons: List[Moon] = []
-    names = ["Io", "Europa", "Ganymede", "Callisto"]
-    for line in lines:
-        match = re.match(pattern, line)
-        if match:
-            moon = Moon(
-                Vec(*[int(coord) for coord in match.groups()]),
-                Vec(0, 0, 0),
-                names.pop(),
-            )
-            moons.append(moon)
-        else:
-            raise Exception(f"Unable to parse {line}")
-
+    # First part
+    moons = parse_moons(lines)
     pairs = list(itertools.combinations(moons, 2))
     for step in range(1000):
 
         # Compute gravity for each pair
         for moon_a, moon_b in pairs:
 
-            if moon_a.pos.x < moon_b.pos.x:
-                moon_a.vel.x += 1
-                moon_b.vel.x -= 1
-            elif moon_a.pos.x > moon_b.pos.x:
-                moon_a.vel.x -= 1
-                moon_b.vel.x += 1
-
-            if moon_a.pos.y < moon_b.pos.y:
-                moon_a.vel.y += 1
-                moon_b.vel.y -= 1
-            elif moon_a.pos.y > moon_b.pos.y:
-                moon_a.vel.y -= 1
-                moon_b.vel.y += 1
-
-            if moon_a.pos.z < moon_b.pos.z:
-                moon_a.vel.z += 1
-                moon_b.vel.z -= 1
-            elif moon_a.pos.z > moon_b.pos.z:
-                moon_a.vel.z -= 1
-                moon_b.vel.z += 1
+            for c in "xyz":
+                if moon_a.pos[c] < moon_b.pos[c]:
+                    moon_a.vel[c] += 1
+                    moon_b.vel[c] -= 1
+                elif moon_a.pos[c] > moon_b.pos[c]:
+                    moon_a.vel[c] -= 1
+                    moon_b.vel[c] += 1
 
         # Apply velocity to each moon
         for moon in moons:
@@ -96,75 +91,41 @@ if __name__ == "__main__":
     total_energy = sum(moon.total_energy for moon in moons)
     assert total_energy == 5517
 
-    pattern = r"<x=([0-9-]+), y=([0-9-]+), z=([0-9-]+)>"
-    moons = []
-    names = ["Io", "Europa", "Ganymede", "Callisto"]
-    for line in lines:
-        match = re.match(pattern, line)
-        if match:
-            moon = Moon(
-                Vec(*[int(coord) for coord in match.groups()]),
-                Vec(0, 0, 0),
-                names.pop(),
-            )
-            moons.append(moon)
-        else:
-            raise Exception(f"Unable to parse {line}")
+    # Second part - each axis is independent so find the cycle independently
+    # for each axis and then compute the lowest common multiple of the cycles
+    moons = parse_moons(lines)
 
-    states = {"x": set(), "y": set(), "z": set()}
-    x_repeated = -1
-    y_repeated = -1
-    z_repeated = -1
+    states: Dict[str, Set[Tuple[str, ...]]] = {"x": set(), "y": set(), "z": set()}
+    repeated = {"x": -1, "y": -1, "z": -1}
 
     pairs = list(itertools.combinations(moons, 2))
     step = 0
     while True:
 
-        x_state = tuple(str(moon.pos.x) + str(moon.vel.x) for moon in moons)
-        if x_state in states["x"]:
-            if x_repeated == -1:
-                x_repeated = step
-        states["x"].add(x_state)
+        # Keep track of state for each axis
+        for c in "xyz":
+            state = tuple(
+                str(getattr(moon.pos, c)) + str(getattr(moon.vel, c)) for moon in moons
+            )
+            if state in states[c]:
+                if repeated[c] == -1:
+                    repeated[c] = step
+            states[c].add(state)
 
-        y_state = tuple(str(moon.pos.y) + str(moon.vel.y) for moon in moons)
-        if y_state in states["y"]:
-            if y_repeated == -1:
-                y_repeated = step
-        states["y"].add(y_state)
-
-        z_state = tuple(str(moon.pos.z) + str(moon.vel.z) for moon in moons)
-        if z_state in states["z"]:
-            if z_repeated == -1:
-                z_repeated = step
-
-        if z_repeated > -1 and y_repeated > -1 and x_repeated > -1:
+        # Cycles found for each xis
+        if all(step > -1 for step in repeated.values()):
             break
-
-        states["z"].add(z_state)
 
         # Compute gravity for each pair
         for moon_a, moon_b in pairs:
 
-            if moon_a.pos.x < moon_b.pos.x:
-                moon_a.vel.x += 1
-                moon_b.vel.x -= 1
-            elif moon_a.pos.x > moon_b.pos.x:
-                moon_a.vel.x -= 1
-                moon_b.vel.x += 1
-
-            if moon_a.pos.y < moon_b.pos.y:
-                moon_a.vel.y += 1
-                moon_b.vel.y -= 1
-            elif moon_a.pos.y > moon_b.pos.y:
-                moon_a.vel.y -= 1
-                moon_b.vel.y += 1
-
-            if moon_a.pos.z < moon_b.pos.z:
-                moon_a.vel.z += 1
-                moon_b.vel.z -= 1
-            elif moon_a.pos.z > moon_b.pos.z:
-                moon_a.vel.z -= 1
-                moon_b.vel.z += 1
+            for c in "xyz":
+                if moon_a.pos[c] < moon_b.pos[c]:
+                    moon_a.vel[c] += 1
+                    moon_b.vel[c] -= 1
+                elif moon_a.pos[c] > moon_b.pos[c]:
+                    moon_a.vel[c] -= 1
+                    moon_b.vel[c] += 1
 
         # Apply velocity to each moon
         for moon in moons:
@@ -172,5 +133,5 @@ if __name__ == "__main__":
 
         step += 1
 
-    lcm_res = lcm(x_repeated, y_repeated, z_repeated)
+    lcm_res = lcm(repeated["x"], repeated["y"], repeated["z"])
     assert lcm_res == 303_070_460_651_184
