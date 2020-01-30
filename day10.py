f5cb110e5abda824108b12983e3d9466e7fe514f
Kalle Lämsä <1397804+coocos@users.noreply.github.com>
2020-01-30 17:30:01 +0200

    Clean up day 10

--- day10.py
+++ day10.py
@@ -3,7 +3,7 @@ import os
 import math
 import collections
 from operator import itemgetter
-from typing import List, DefaultDict, Tuple, Set
+from typing import List, DefaultDict, Tuple, Set, Dict
 from dataclasses import dataclass
 
 
@@ -24,6 +24,15 @@ class Vector:
         # the greatest common divisor and divide components by it
         gcd = abs(math.gcd(self.x, self.y))
         return Vector(self.x // gcd, self.y // gcd)
+
+
+def dot(vec1: Tuple[float, float], vec2: Tuple[float, float]) -> float:
+    return vec1[0] * vec2[0] + vec1[1] * vec2[1]
+
+
+def angle(vec: Tuple[float, float]) -> Tuple[float, float]:
+    norm = math.sqrt(vec[0] ** 2 + vec[1] ** 2)
+    return (vec[0] / norm, vec[1] / norm)
 
 
 def parse_asteroids(grid: List[List[str]]) -> List[Vector]:
@@ -73,10 +82,10 @@ def group_asteroids(station: Vector, asteroids: List[Vector]) -> DefaultDict:
 
 if __name__ == "__main__":
 
-    # First part
     with open(os.path.join("inputs", "day10.in")) as f:
         grid = [list(line.strip()) for line in f.readlines()]
 
+    # First part
     asteroids = parse_asteroids(grid)
     visibility = [
         (asteroid, count_visible_asteroids(asteroid, asteroids))
@@ -89,7 +98,8 @@ if __name__ == "__main__":
     # Second part
     groups = group_asteroids(station, asteroids)
 
-    quadrants = {"tr": [], "br": [], "bl": [], "tl": []}
+    # Divide asteroid groups into 4 quadrants based on their direction from the station
+    quadrants: Dict[str, List] = {"tr": [], "br": [], "bl": [], "tl": []}
     for direction in groups:
         if direction[0] >= 0:
             if direction[1] < 0:
@@ -102,16 +112,18 @@ if __name__ == "__main__":
             else:
                 quadrants["bl"].append(direction)
 
-    # Oh dear
-    quadrants["tr"].sort(key=lambda d: math.atan2(-d[1], d[0]), reverse=True)
-    quadrants["br"].sort(key=lambda d: math.atan2(d[1], d[0]))
-    quadrants["bl"].sort(key=lambda d: math.atan2(d[1], -d[0]), reverse=True)
-    quadrants["tl"].sort(key=lambda d: math.atan2(-d[1], d[0]), reverse=True)
+    # Sort asteroids in each quadrant based on dot product / angle with each axis vector
+    quadrants["tr"].sort(key=lambda d: dot(angle(d), (0, -1)))
+    quadrants["br"].sort(key=lambda d: dot(angle(d), (1, 0)))
+    quadrants["bl"].sort(key=lambda d: dot(angle(d), (0, 1)))
+    quadrants["tl"].sort(key=lambda d: dot(angle(d), (1, 0)))
 
+    # Combine quadrants to form a list of asteroid groups in a circular order
     asteroid_groups = (
         quadrants["tr"] + quadrants["br"] + quadrants["bl"] + quadrants["tl"]
     )
 
+    # Blast away at the sorted asteroids until 200 have been vaporized
     vaporized: List[Vector] = []
     for group in asteroid_groups:
         asteroid, _ = groups[group].pop()
